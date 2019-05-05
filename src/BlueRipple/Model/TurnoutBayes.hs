f46478d092d102b33803fbeb98117914458f2180
adamConnerSax <adam_conner_sax@yahoo.com>
2019-05-04 20:58:08 -0400

    Hamiltonian MC?

--- src/BlueRipple/Model/TurnoutBayes.hs
+++ src/BlueRipple/Model/TurnoutBayes.hs
@@ -9,16 +9,16 @@ import           Numeric.MathFunctions.Constants
                                                 ( m_ln_sqrt_2_pi )
 --import qualified Numeric.MCMC.Flat             as MC
 import qualified Numeric.MCMC                  as MC
---import Numeric.AD (grad)
+import           Numeric.AD                     ( grad )
 import           Math.Gamma                     ( gamma )
 
 data ObservedVote = ObservedVote { dem :: Int}
 
 data Pair a b = Pair !a !b
 
-logProbObservedVote :: V.Vector Double -> Int -> [Int] -> Double
+--logProbObservedVote :: V.Vector Double -> Int -> [Int] -> Double
 logProbObservedVote !demProbs !demVote !turnoutCounts =
-  let np          = zip turnoutCounts (V.toList demProbs)
+  let np          = zip turnoutCounts (demProbs)
       foldMeanVar = FL.Fold
         (\(Pair m v) (n, p) ->
           let m' = (realToFrac n) * p in (Pair (m + m') (v + m' * (1 - p)))
@@ -28,19 +28,19 @@ logProbObservedVote !demProbs !demVote !turnoutCounts =
       Pair m v = FL.fold foldMeanVar np
   in  negate $ log v + ((realToFrac demVote - m) ^ 2 / (2 * v))
 
-logProbObservedVotes :: [(Int, [Int])] -> V.Vector Double -> Double
+--logProbObservedVotes :: [(Int, [Int])] -> V.Vector Double -> Double
 logProbObservedVotes votesAndTurnout demProbs =
   FL.fold FL.sum $ fmap (uncurry $ logProbObservedVote demProbs) votesAndTurnout
 
-betaDist :: Double -> Double -> Double -> Double
+--betaDist :: Double -> Double -> Double -> Double
 betaDist alpha beta x =
-  let b = gamma (alpha + beta) / (gamma alpha + gamma beta)
+  let b = 1 -- gamma (alpha + beta) / (gamma alpha + gamma beta)
   in  if (x >= 0) && (x <= 1)
         then b * (x ** (alpha - 1)) * ((1 - x) ** (beta - 1))
         else 0
 
-betaPrior :: Double -> Double -> V.Vector Double -> Double
-betaPrior a b xs = V.product $ V.map (betaDist a b) xs
+--betaPrior :: Double -> Double -> V.Vector Double -> Double
+betaPrior a b xs = FL.fold FL.product $ fmap (betaDist a b) xs
 
 {-
 origin :: MC.Ensemble
@@ -50,22 +50,22 @@ origin = MC.ensemble
   ]
 -}
 
-f :: [(Int, [Int])] -> [Double] -> Double
+--f :: [(Int, [Int])] -> [Double] -> Double
 f votesAndTurnout demProbs =
-  let v = V.fromList demProbs
+  let v = demProbs
   in  (exp $ logProbObservedVotes votesAndTurnout v) * (betaPrior 2 2 v)
 
-fLog :: [(Int, [Int])] -> [Double] -> Double
+--fLog :: [(Int, [Int])] -> [Double] -> Double
 fLog votesAndTurnout demProbs =
-  let v = V.fromList demProbs
+  let v = demProbs
   in  logProbObservedVotes votesAndTurnout v + log (betaPrior 2 2 v)
 
 --gFLog :: [(Int, [Int])] -> [Double] -> [Double]
---gFLog votesAndTurnout = grad (\x -> fLog votesAndTurnout x)
+gFLog votesAndTurnout = grad (fLog votesAndTurnout)
 
 runMCMC votesAndTurnout numIters initialProb stepSize invTemp =
   MC.withSystemRandom . MC.asGenIO $ MC.chain
     numIters
     (replicate 8 initialProb)
-    (MC.anneal invTemp $ MC.metropolis stepSize)
-    (MC.Target (fLog votesAndTurnout) Nothing)
+    (MC.frequency [(9, MC.metropolis stepSize), (1, MC.hamiltonian 0.01 20)])
+    (MC.Target (fLog votesAndTurnout) (Just $ gFLog votesAndTurnout))
