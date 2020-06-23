ea0a7cf8fe0beb5712df33d7d7c64fca354b5295
adamConnerSax <adam_conner_sax@yahoo.com>
2020-06-23 14:42:21 -0400

    made mrpPrefs use concurrency again.

--- reports/MRP/CCES_MRP_Analysis.hs
+++ reports/MRP/CCES_MRP_Analysis.hs
@@ -267,16 +267,14 @@ mrpPrefs mdv cacheTmpDirM cachedCCES_Data predictor catPredMap = do
                 )
                 [2008, 2010, 2012, 2014, 2016, 2018]
       allActions = [p2008, p2012, p2016] ++ pHouse
-      
+{-      
   allResults <- sequence allActions
   return $ mconcat allResults
-
-{-      
+-}
   allResultsM <- sequence <$> K.sequenceConcurrently allActions
   case allResultsM of
     Nothing -> K.knitError "Error in MR run (mrpPrefs)."
     Just allResults -> return $ mconcat allResults
--}
 
 {-
 return
