7269a3868345628e3d33d048915074a2653bd047
adamConnerSax <adam_conner_sax@yahoo.com>
2020-10-13 21:17:38 -0400

    Working in parameter indexing

--- src/Stan/Parameters.hs
+++ src/Stan/Parameters.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -10,47 +11,60 @@ module Stan.Parameters where
 import qualified Data.Map as M
 import qualified Data.Vector as V
 
-import qualified GHC.TypeNats as Nat
+--import qualified GHC.TypeNats as Nat
 
-type family NTuple (n :: Nat.Nat) where
-  NTuple 0 = ()
-  NTuple 1 = Int
-  NTuple 2 = (Int, Int)
-  NTuple 3 = (Int, Int, Int)
-  NTuple 4 = (Int, Int, Int, Int)
+data Dim = D0 | D1 | D2 | D3 | D4
 
-class Nat.KnownNat n => IndexFunction n where
+type family NTuple (n :: Dim) where
+  NTuple D0 = ()
+  NTuple D1 = Int
+  NTuple D2 = (Int, Int)
+  NTuple D3 = (Int, Int, Int)
+  NTuple D4 = (Int, Int, Int, Int)
+
+class IndexFunction (n :: Dim) where
   tupleToIndex :: NTuple n -> NTuple n -> Int
   indexToTuple :: NTuple n -> Int -> NTuple n
 
-instance (IndexFunction 0) where
-  tupleToIndex = undefined
-  indexToTuple = undefined
-
-instance (IndexFunction 1) where
+instance IndexFunction D0 where
+  tupleToIndex _ _ = 0
+  indexToTuple _ _ = ()
+  
+instance IndexFunction D1 where
   tupleToIndex _ = id
   indexToTuple _ = id 
 
-instance (IndexFunction 2) where
+instance IndexFunction D2 where
   tupleToIndex (_, nj) (i, j) = i * nj + j  
   indexToTuple (_, nj) n = let j = n `mod` nj in ((n - j) `div` nj, j)
 
-instance (IndexFunction 3) where
-  tupleToIndex (ni, nj, nk) (i, j, k) =  tupleToIndex @2 (ni, nj) (i, j) * nk + k
+instance IndexFunction D3 where
+  tupleToIndex (ni, nj, nk) (i, j, k) =  tupleToIndex @D2 (ni, nj) (i, j) * nk + k
   indexToTuple (ni, nj, nk) n =
     let k = n `mod` nk
         m = (n - k) `div` nk
-        (i, j) = indexToTuple @2 (ni, nj) m
+        (i, j) = indexToTuple @D2 (ni, nj) m
     in (i, j, k)
 
---instance (IndexFunction 4) where
---  tupleToIndex (_, nj, nk, nl) = ((i * nj + j) * nk + k) * nl + l
---  indexToTuple 
+instance IndexFunction D4 where
+  tupleToIndex (ni, nj, nk, nl) (i, j, k, l) = tupleToIndex @D3 (ni, nj, nk) (i, j, k) * nl + l
+  indexToTuple (ni, nj, nk, nl) n =
+    let l = n `mod` nl
+        m = (n - l) `div` nl
+        (i, j, k) = indexToTuple @D3 (ni, nj, nk) m
+    in (i, j, k, l)
 
 
-{-
-data ParameterStatistics a where
-  ScalarStatistics :: a -> ParameterStatistics
-  IndexedStatistics :: KnownNat n => Int -> (
+data ParameterStatistics (dim :: Dim) a where
+  ParameterStatistics :: NTuple dim -> V.Vector a -> ParameterStatistics dim a
+  
+getIndexed :: forall dim a. IndexFunction dim => ParameterStatistics dim a -> NTuple dim -> a
+getIndexed (ParameterStatistics dims vec) index = vec V.! tupleToIndex @dim dims index 
 
--}
+getScalar :: ParameterStatistics D0 a -> a
+getScalar p@(ParameterStatistics _ v) = getIndexed p ()
+
+data ParameterStatisticsA a where
+  ParameterStatisticsA :: (forall dim. ParameterStatistics dim a) -> ParameterStatisticsA a
+
+--parameterStatistics :: ParameterStatistics a -> NTuple
