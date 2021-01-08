0dc3d0fb9795695fd815d3d50d623696544f5933
adamConnerSax <adam_conner_sax@yahoo.com>
2021-01-08 08:58:24 -0500

    Changed structure of Frames-hvega and thus cabal,project

--- stan/Main.hs
+++ stan/Main.hs
@@ -101,27 +101,28 @@ testHouseModel = do
   K.logLE K.Info "(predictors.html): Predictor & Predicted: Distributions & Correlations"
   K.newPandoc (K.PandocInfo "examine_predictors" mempty) $ do
     BR.brAddMarkDown "## Predictors/Predicted: Distributions in house elections 2012-2018"
-    let vcDist = FV.ViewConfig 400 400 5
+    let vcDist = FV.ViewConfig 200 200 5
+        mhStyle = FV.FacetedBar
         votes r = F.rgetField @BRE.DVotes r + F.rgetField @BRE.RVotes r
         turnout r = realToFrac (votes r) / realToFrac (F.rgetField @PUMS.Citizens r)
         dShare r = if votes r > 0 then realToFrac (F.rgetField @BRE.DVotes r) / realToFrac (votes r) else 0
     _ <- K.addHvega Nothing Nothing
-         $ FV.multiHistogram @BR.Year @BRE.FracUnder45 "% Under 45" Nothing 50 FV.DataMinMax True FV.AdjacentBar vcDist (hmd ^. #electionData)
+         $ FV.multiHistogram @BRE.FracUnder45 @BR.Year "% Under 45" Nothing 50 FV.DataMinMax True mhStyle vcDist (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @BRE.FracFemale "% Female" Nothing 50 FV.DataMinMax True vcDist (hmd ^. #electionData)
+         $ FV.multiHistogram @BRE.FracFemale @BR.Year "% Female" Nothing 50 FV.DataMinMax True mhStyle vcDist (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @BRE.FracGrad "% Grad" Nothing 50 FV.DataMinMax True vcDist (hmd ^. #electionData)
+         $ FV.multiHistogram @BRE.FracGrad @BR.Year "% Grad" Nothing 50 FV.DataMinMax True  mhStyle vcDist (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @BRE.FracNonWhite "% Non-White" Nothing 50 FV.DataMinMax True vcDist (hmd ^. #electionData)
+         $ FV.multiHistogram @BRE.FracNonWhite @BR.Year "% Non-White" Nothing 50 FV.DataMinMax True mhStyle vcDist (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @DT.AvgIncome "Average Income" Nothing 50 FV.DataMinMax True vcDist (hmd ^. #electionData)
+         $ FV.multiHistogram @DT.AvgIncome @BR.Year "Average Income" Nothing 50 FV.DataMinMax True mhStyle vcDist (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @DT.PopPerSqMile "Density (ppl/sq mile)" Nothing 50 FV.DataMinMax True vcDist (hmd ^. #electionData)
+         $ FV.multiHistogram @DT.PopPerSqMile @BR.Year "Density (ppl/sq mile)" Nothing 50 FV.DataMinMax True mhStyle vcDist (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @PctTurnout "Turnout %" Nothing 50 FV.DataMinMax True vcDist
+         $ FV.multiHistogram @PctTurnout @BR.Year "Turnout %" Nothing 50 FV.DataMinMax True mhStyle vcDist
          $ FT.mutate (FT.recordSingleton @PctTurnout . (*100) . turnout) <$> (hmd ^. #electionData)
     _ <- K.addHvega Nothing Nothing
-         $ FV.singleHistogram @DShare "Dem Share" Nothing 50 FV.DataMinMax True vcDist
+         $ FV.multiHistogram @DShare @BR.Year "Dem Share" Nothing 50 FV.DataMinMax True mhStyle vcDist
          $ FT.mutate (FT.recordSingleton @DShare . (*100). dShare) <$> (hmd ^. #electionData)
     BR.brAddMarkDown "## Predictors/Predicted: Correlations in house elections 2012-2018"
     let corrSet = S.fromList [FV.LabeledCol "% Under 45" (F.rgetField @BRE.FracUnder45)
