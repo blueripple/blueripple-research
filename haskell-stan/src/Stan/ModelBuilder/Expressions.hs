63f910f5a6f6fb3f0bd6f0eef351d23defb91efb
adamConnerSax <adam_conner_sax@yahoo.com>
2021-04-05 16:54:30 -0400

    Working recursion-schemes expressions

--- haskell-stan/src/Stan/ModelBuilder/Expressions.hs
+++ haskell-stan/src/Stan/ModelBuilder/Expressions.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
@@ -17,9 +19,12 @@
 module Stan.ModelBuilder.Expressions where
 
 
-import Prelude hiding (All)
+import Prelude hiding (All, group)
 import qualified Control.Foldl as Fold
-import qualified Data.Functor.Base as Rec
+import qualified Data.Functor.Foldable as Rec
+import qualified Data.Functor.Foldable.TH as Rec
+
+import qualified Data.Fix as Fix
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 
@@ -28,8 +33,7 @@ data StanDim = NamedDim StanName | GivenDim Int deriving (Show, Eq, Ord)
 
 dimToText :: StanDim -> Text
 dimToText (NamedDim t) = t
-cdimToText (GivenDim n) = show n
-
+dimToText (GivenDim n) = show n
 
 data StanType = StanInt
               | StanReal
@@ -39,83 +43,107 @@ data StanType = StanInt
 
 data StanVar = StanVar StanName StanType
 
-data VectorContext = Vectorized | NonVectorized Text
+data VectorContext = Vectorized | NonVectorized Text deriving (Show, Eq)
 
-data StanModelTerm where
-  Scalar :: Text -> StanModelTerm
-  Vectored :: Text -> StanModelTerm
-  Indexed :: Text -> Text -> StanModelTerm
-
-printTerm :: Map Text Text -> VectorContext -> StanModelTerm -> Either Text Text
-printTerm _ _ (Scalar t) = Right t
-printTerm _ Vectorized (Vectored n) = Right n
-printTerm _ (NonVectorized i) (Vectored n) = Right $ n <> "[" <> i <> "]"
-printTerm indexMap _ (Indexed k t) = (\i -> t <> "[" <> i <> "]")
-                                     <$> (maybe
-                                     (Left
-                                       $ "Failed to find key="
-                                       <> k
-                                       <> " in "
-                                       <> show indexMap
-                                       <> " when indexing an expression.")
-                                     Right
-                                     $ Map.lookup k indexMap)
-
---data Indexed = Simple | ReIndexed Text
+data StanExprF a where
+  ScalarF :: Text -> StanExprF a
+  IndexedF :: Text -> StanExprF a
+  ReIndexedF :: Text -> Text -> StanExprF a
+  BinOpF :: Text -> a -> a -> StanExprF a
+  FunctionF :: Text -> [a] -> StanExprF a
+  VectorFunctionF :: Text -> a -> StanExprF a -- function to add when the context is vectorized
+  GroupF :: Text -> Text -> a -> StanExprF a
+  ArgsF :: [a] -> StanExprF a
+--  ArrayF :: [a] -> StanExprF a
+  deriving (Show, Eq, Functor, Foldable, Traversable)
 
 
-data StanExpr where
---  NullE :: StanExpr
-  TermE :: StanModelTerm -> StanExpr --StanModelTerm
-  BinOpE :: Text -> StanExpr -> StanExpr -> StanExpr
-  FunctionE :: Text -> [StanExpr] -> StanExpr
-  VectorFunctionE :: Text -> StanExpr -> StanExpr -- function to add when the context is vectorized
-  GroupE :: Text -> Text -> StanExpr -> StanExpr
-  ArgsE :: [StanExpr] -> StanExpr
-  ArrayE :: [StanExpr] -> StanExpr
+type StanExpr = Fix.Fix StanExprF
 
+scalar :: Text -> StanExpr
+scalar = Fix.Fix . ScalarF
 
-termE :: StanModelTerm -> StanExpr
-termE = TermE
+indexed :: Text -> StanExpr
+indexed = Fix.Fix . IndexedF
 
-binOpE :: Text -> StanExpr -> StanExpr -> StanExpr
-binOpE = BinOpE
+reIndexed :: Text -> Text -> StanExpr
+reIndexed k t = Fix.Fix $ ReIndexedF k t
 
-functionE :: Text -> [StanExpr] -> StanExpr
-functionE = FunctionE
+binOp :: Text -> StanExpr -> StanExpr -> StanExpr
+binOp op e1 e2 = Fix.Fix $ BinOpF op e1 e2
 
-functionWithGivensE :: Text -> [StanExpr] -> [StanExpr] -> StanExpr
-functionWithGivensE fName gs as = functionE fName [binOpE "|" (argsE gs) (argsE as)]
+function :: Text -> [StanExpr] -> StanExpr
+function fName es = Fix.Fix $ FunctionF fName es
 
-vectorFunctionE :: Text -> StanExpr -> StanExpr
-vectorFunctionE = VectorFunctionE
+functionWithGivens :: Text -> [StanExpr] -> [StanExpr] -> StanExpr
+functionWithGivens fName gs as = function fName [binOp "|" (args gs) (args as)]
 
-groupE ::  Text -> Text -> StanExpr -> StanExpr
-groupE = GroupE
+vectorFunction :: Text -> StanExpr -> StanExpr
+vectorFunction t = Fix.Fix . VectorFunctionF t
 
-parenE :: StanExpr -> StanExpr
-parenE = groupE "(" ")"
+group ::  Text -> Text -> StanExpr -> StanExpr
+group ld rd = Fix.Fix . GroupF ld rd
 
-bracketE :: StanExpr -> StanExpr
-bracketE = groupE "{" "}"
+args :: [StanExpr] -> StanExpr
+args = Fix.Fix . ArgsF
 
-argsE :: [StanExpr] -> StanExpr
-argsE = ArgsE
+paren :: StanExpr -> StanExpr
+paren = group "(" ")"
 
+bracket :: StanExpr -> StanExpr
+bracket = group "{" "}"
+
+--args :: [StanExpr] -> StanExpr
+--args = Fix.Fix . ArgsE . fmap Fix.unFix
 
 eqE ::  StanExpr -> StanExpr -> StanExpr
-eqE = binOpE "="
+eqE = binOp "="
 
 vectorSampleE :: StanExpr -> StanExpr -> StanExpr
-vectorSampleE = binOpE "~"
+vectorSampleE = binOp "~"
 
 plusE ::  StanExpr -> StanExpr -> StanExpr
-plusE = binOpE "+"
+plusE = binOp "+"
 
 timesE :: StanExpr -> StanExpr -> StanExpr
-timesE = binOpE "*"
+timesE = binOp "*"
 
+multiOp :: Text -> NonEmpty StanExpr -> StanExpr
+multiOp o es = foldl' (binOp o) (head es) (tail es)
 
+-- This is wild
+reIndexExpression :: Map Text Text -> StanExpr -> Either Text StanExpr
+reIndexExpression im = Rec.cata alg where
+  reIndex :: Map Text Text -> Text -> Text -> Either Text Text
+  reIndex im key term = case Map.lookup key im of
+    Nothing -> Left $ "re-indexing key \"" <> key <> "\" not found in index-map: " <> show im
+    Just i -> Right $ term <> "[" <> i <> "]"
+  alg :: StanExprF (Either Text StanExpr) -> Either Text StanExpr
+  alg (ReIndexedF k t) = case Map.lookup k im of
+    Nothing -> Left $ "re-indexing key \"" <> k <> "\" not found in index-map: " <> show im
+    Just i -> Right $ indexed $ t <> "[" <> i <> "]"
+  alg x = Fix.Fix <$> sequenceA x
+
+printReIndexedExpr :: VectorContext -> StanExpr -> Text
+printReIndexedExpr vc = Rec.cata alg where
+  printVC :: VectorContext -> Text -> Text
+  printVC Vectorized t = t
+  printVC (NonVectorized i) t = t <> "[" <> i <> "]"
+  csArgs es = T.intercalate ", " es
+  alg :: StanExprF Text -> Text
+  alg (ScalarF t) = t
+  alg (IndexedF t) = printVC vc t
+  alg (ReIndexedF k t) = error "Should not call printExpr' before reIndexing" --printVC vc <$> reIndex im k t
+  alg (BinOpF op e1 e2) = e1 <> " " <> op <> " " <> e2
+  alg (FunctionF f es) = f <> "(" <> csArgs es <> ")"
+  alg (VectorFunctionF f e) = if vc == Vectorized then f <> "(" <> e <> ")" else e
+  alg (GroupF ld rd e) = ld <> e <> rd
+  alg (ArgsF es) = csArgs es
+
+printExpr :: Map Text Text -> VectorContext -> StanExpr -> Either Text Text
+printExpr im vc expr = printReIndexedExpr vc <$> reIndexExpression im expr
+
+{-
 printExpr :: Map Text Text -> VectorContext -> StanExpr -> Either Text Text
 --printExpr _ _ NullE = Just ""
 printExpr im vc (TermE t) = printTerm im vc t
@@ -125,6 +153,4 @@ printExpr im Vectorized (VectorFunctionE f e) = (\e' -> f <> "(" <>  e' <> ")") 
 printExpr im vc@(NonVectorized _) (VectorFunctionE f e) = printExpr im vc e
 printExpr im vc (GroupE ld rd e) = (\e' -> ld <> e' <> rd) <$> printExpr im vc e
 printExpr im vc (ArgsE es) = T.intercalate ", " <$> traverse (printExpr im vc) es
-
-multiOp :: Text -> NonEmpty StanExpr -> StanExpr
-multiOp o es = foldl' (BinOpE o) (head es) (tail es)
+-}
