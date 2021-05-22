435d45da420963dc551a314363655c530b701331
adamConnerSax <adam_conner_sax@yahoo.com>
2021-05-21 21:04:42 -0400

    Working on path stuff

--- src/BlueRipple/Configuration.hs
+++ src/BlueRipple/Configuration.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings         #-}
 {-# LANGUAGE QuasiQuotes               #-}
 {-# LANGUAGE ScopedTypeVariables       #-}
@@ -64,18 +65,62 @@ brLocalRoot = "posts/"
 -- 1. Putting post documents in the right place in a tree for both draft and post
 -- 2. Support Unused bits, existing only in dev.
 -- 3. Help forming the correct URLs for links in either case
-data PostPaths = PostPaths { inputsDir :: Path Abs Dir -- executable relative or absolute
-                           , draftHtmlDir :: Path Abs Dir -- executable relative or absolute
-                           , postHtmlDir :: Path Abs Dir -- executable relative or absolute
-                           }
+data Output = Draft | Post
+data PostPaths a = PostPaths { inputsDir :: Path a Dir -- place to put inputs (markDown, etc.)
+                             , draftHtmlDir :: Path a Dir --
+                             , postHtmlDir :: Path a Dir -- local html location, to be pushed to github pages
+                             , postUrlRoute :: Path Abs Dir -- URL root for post links, without "https:/"
+                             }
 
-markDownPath :: PostPaths -> Path Abs Dir
+
+markDownPath :: PostPaths a -> Path a Dir
 markDownPath pp = inputsDir pp Path.</> [Path.reldir|md|]
 
-notesMDPath ::  PostPaths -> Text -> Either SomeException (Path Abs Dir)
+notesMDPath ::  PostPaths a -> Text -> Either Text (Path a File)
 notesMDPath pp noteName =
-  fmap (\s -> markDownPath pp </> [Path.reldir|Notes|] </> s) $ Path.parseRelDir $ toString noteName
+  first show
+  $ fmap (\s -> markDownPath pp </> [Path.reldir|Notes|] </> s) $ Path.parseRelFile $ toString noteName
 
-unusedMDPath ::  PostPaths -> Text -> Either SomeException (Path Abs Dir)
+unusedMDPath ::  PostPaths a -> Text -> Either Text (Path a File)
 unusedMDPath pp uName =
-  fmap (\s -> markDownPath pp </> [Path.reldir|Unused|] </> s) $ Path.parseRelDir $ toString uName
+  first show
+  $ fmap (\s -> markDownPath pp </> [Path.reldir|Unused|] </> s) $ Path.parseRelFile $ toString uName
+
+postPath :: PostPaths a -> Output -> Path a File
+postPath pp = \case
+  Draft -> draftHtmlDir pp </> [Path.relfile|post.html|]
+  Post -> postHtmlDir pp </> [Path.relfile|post.html|]
+
+absPostPaths :: Path Abs Dir -> PostPaths Rel -> PostPaths Abs
+absPostPaths s (PostPaths i d p r) = PostPaths (s </> i) (s </> d) (s </> p) r
+
+defaultLocalRoot :: Path Abs Dir
+defaultLocalRoot = [Path.absdir|/Users/adam/BlueRipple|]
+
+resDir :: Path Rel Dir
+resDir = [Path.reldir|research|]
+
+gpDir :: Path Rel Dir
+gpDir = [Path.reldir||blueripple.github.io|]
+
+postPaths :: Path Abs Dir -> Path Rel Dir -> Path Rel Dir -> Path Rel Dir -> Path Rel Dir -> PostPaths Abs
+postPaths localRoot iP dhP phP relRoute =
+  absPostPaths localRoot
+  $ PostPaths
+    (resDir </> iP)
+    (resDir </> dhP)
+    (gpDir </> phP)
+    ([Path.absdir|/blueripplepolitics.org/|] </> relRoute)
+
+
+noteRelDir :: Path Rel Dir
+noteRelDir = [Path.reldir|Notes|]
+
+noteUrl :: PostPaths Abs -> Output -> Text -> Either Text Text
+noteUrl pp o noteName = do
+  noteNameRelFile <- first show $ Path.parseRelFile (toString $ noteName <> ".html")
+  let noteRelFile :: Path Rel File = noteRelDir </> noteNameRelFile
+      noteUrl = case o of
+        Draft -> Path.toFilePath noteRelFile
+        Post -> "https:/" <> Path.toFilePath (postUrlRoute pp </> noteRelFile)
+  return $ toText noteUrl
