1cab2b3ff5dc9ccb1c1e3805c9b41b26b3b7666f
adamConnerSax <adam_conner_sax@yahoo.com>
2022-01-02 16:32:05 -0500

    Working on SamplerCSV

--- haskell-stan/src/Stan/SamplerCSV.hs
+++ haskell-stan/src/Stan/SamplerCSV.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Stan.SamplerCSV where
 
-import Prelude hiding (many)
+import Prelude hiding (many, some)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
+
+import qualified Data.Massiv.Vector as MV
+import qualified Data.Massiv.Array as M
+
+import qualified Data.Text as T
+
 --import Control.Applicative.Combinators
 
 -- parse stan csv formats.
@@ -25,11 +32,60 @@ commentLine = (\c t e -> toText ([c] ++ t) <> e) <$> char '#' <*> many alphaNumC
 commentSection :: Parser Text
 commentSection = fmap mconcat $ some commentLine
 
-commaFirst :: Parser a -> Parser a
-commaFirst p = flip const <$> char ',' <*> p
+untilComma :: Parser Text
+untilComma = takeWhileP (Just "non-comma") (/= ',')
+
+--commaFirst :: Parser a -> Parser a
+--commaFirst p = flip const <$> char ',' <*> p
 
 csvLine :: Parser a -> Parser [a]
-csvLine p = (\h t _ -> h:t) <$> p <*> many (commaFirst p) <*> eol
+csvLine p = sepEndBy1 p (void (char ',') <|> void eol)
 
 headerLine :: Parser [Text]
-headerLine = csvLine
+headerLine = csvLine untilComma
+
+dataLine :: Parser [Double]
+dataLine = csvLine L.float
+
+type Samples = M.Array M.U M.Ix2 Double
+
+samples :: Parser Samples
+samples = M.fromLists' M.Seq <$> many dataLine
+
+data SamplerCSV = SamplerCSV
+  { samplerDetails :: Text
+  , adaptDetails :: Text
+  , timeDetails :: Text
+  , samplerHeader :: [Text]
+  , samplerSamples :: Samples
+  }
+
+samplerCSV :: Parser SamplerCSV
+samplerCSV = do
+  sDetails <- commentSection
+  header <- headerLine
+  aDetails <- commentSection
+  sSamples <- samples
+  tDetails <- commentSection
+  eol
+  return $ SamplerCSV sDetails aDetails tDetails header sSamples
+
+data GQCSV = GQCSV
+  {
+    gqDetails :: Text
+  , gqHeader :: [Text]
+  , gqSamples :: Samples
+  }
+
+gqCSV :: Parser GQCSV
+gqCSV = do
+  details <- commentSection
+  header <- headerLine
+  gqSamples <- samples
+  return $ GQCSV details header gqSamples
+
+headerText :: [Text] -> Text
+headerText = T.intercalate ","
+
+samplesText :: Samples -> [Text]
+samplesText = undefined
