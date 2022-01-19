c1b487933e9003893716ab5b6cab871ff83675c5
adamConnerSax <adam_conner_sax@yahoo.com>
2022-01-18 19:18:18 -0500

    Working on getting FEC-data working again...

--- FEC-data/Main.hs
+++ FEC-data/Main.hs
@@ -9,5 +9,5 @@ import qualified Data.Time.Calendar as Calendar
 main :: IO ()
 main= do
 --  BR.FromDB.netSpendingBySenateCandidatesBetweenCSV Nothing (Calendar.fromGregorian 2020 8 27)
---  BR.FromDB.netSpendingByHouseCandidatesBetweenCSV Nothing (Calendar.fromGregorian 2020 8 27)
-  FromDB.moneySummaryCSV
+  BR.FromDB.netSpendingByHouseCandidatesBetweenCSV (Just $ Calendar.fromGregorian 2021 01 01) (Calendar.fromGregorian 2022 1 15)
+--  FromDB.moneySummaryCSV
