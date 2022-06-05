d58958d302a9abb234968c729a752df052fd0ff7
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2022-06-04 20:55:46 -0400

    Working on Program module

--- haskell-stan/src/Stan/ModelBuilder/TypedExpressions/Indexing.hs
+++ haskell-stan/src/Stan/ModelBuilder/TypedExpressions/Indexing.hs
@@ -5,61 +5,65 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE StandaloneKindSignatures #-}
 
 module Stan.ModelBuilder.TypedExpressions.Indexing
-  (
-    module Stan.ModelBuilder.TypedExpressions.Indexing
-  , Fin(..)
-  , Vec(..)
+  ( module Stan.ModelBuilder.TypedExpressions.Indexing,
+    Fin (..),
+    Vec (..),
   )
-  where
+where
 
+import Data.Fin (Fin (..))
+import Data.Kind (Type)
+import qualified Data.Type.Nat as DT
+import Data.Vec.Lazy (Vec (..))
+import qualified Data.Vec.Lazy as DT
+import GHC.TypeLits (ErrorMessage ((:<>:)))
+import qualified GHC.TypeLits as TE
 import qualified Stan.ModelBuilder.TypedExpressions.Recursion as TR
 import Stan.ModelBuilder.TypedExpressions.Types
-
 import Prelude hiding (Nat)
-import           Data.Kind (Type)
-
-import Data.Fin (Fin(..))
-import Data.Vec.Lazy (Vec(..))
-
-import qualified Data.Type.Nat as DT
-import qualified Data.Vec.Lazy as DT
-
-import qualified GHC.TypeLits as TE
-import GHC.TypeLits (ErrorMessage((:<>:)))
 
 -- to simplify most indexing
 -- term level
 n0 :: Nat = Z
+
 n1 :: Nat = S Z
+
 n2 :: Nat = S n1
+
 n3 :: Nat = S n2
 
 -- type level
 type N0 :: Nat
 type N0 = Z
+
 type N1 :: Nat
 type N1 = S Z
+
 type N2 :: Nat
 type N2 = S (S Z)
+
 type N3 :: Nat
 type N3 = S (S (S Z))
 
 s0 :: SNat Z = DT.SZ
+
 s1 :: SNat (S Z) = DT.SS
+
 s2 :: SNat (S (S Z)) = DT.SS
+
 s3 :: SNat (S (S (S Z))) = DT.SS
 
---popRandom :: forall n m a. (DT.SNatI n, DT.SNatI m) => Vec (DT.Plus n (S m)) a -> (a, Vec (DT.Plus n m) a)
---popRandom v = (a, vL DT.++ vR)
+-- popRandom :: forall n m a. (DT.SNatI n, DT.SNatI m) => Vec (DT.Plus n (S m)) a -> (a, Vec (DT.Plus n m) a)
+-- popRandom v = (a, vL DT.++ vR)
 --  where (vL, a ::: vR) = DT.split v :: (Vec n a, Vec (S m) a)
 
 type family DiffOrZero (n :: Nat) (m :: Nat) :: Nat where
@@ -74,7 +78,7 @@ type family DeclDimension (e :: EType) :: Nat where
   DeclDimension EComplex = Z
   DeclDimension ECVec = S Z
   DeclDimension ERVec = S Z
-  DeclDimension EMat  = S (S Z)
+  DeclDimension EMat = S (S Z)
   DeclDimension ESqMat = S Z
   DeclDimension (EArray n t) = n `DT.Plus` Dimension t
 
@@ -89,7 +93,7 @@ type family Dimension (e :: EType) :: Nat where
   Dimension (EArray n t) = n `DT.Plus` Dimension t
 
 type family ApplyDiffOrZeroToEType (n :: Nat) (e :: EType) :: EType where
-  ApplyDiffOrZeroToEType Z (EArray Z t)  = TE.TypeError (TE.Text "Attempt to slice a zero-dimensional array.  Which means you had a zero dimensional array?")
+  ApplyDiffOrZeroToEType Z (EArray Z t) = TE.TypeError (TE.Text "Attempt to slice a zero-dimensional array.  Which means you had a zero dimensional array?")
   ApplyDiffOrZeroToEType Z (EArray (S Z) t) = t
   ApplyDiffOrZeroToEType Z (EArray (S (S n)) t) = EArray (S n) t
   ApplyDiffOrZeroToEType d (EArray m t) = EArray m (Sliced d t)
@@ -115,7 +119,7 @@ type family SliceInnerN (n :: Nat) (a :: EType) :: EType where
   SliceInnerN Z a = a
   SliceInnerN (S n) a = SliceInnerN n (Sliced Z a)
 
-newtype DeclIndexVecF (r :: EType -> Type) (et :: EType) = DeclIndexVecF { unDeclIndexVecF :: Vec (DeclDimension et) (r EInt)}
+newtype DeclIndexVecF (r :: EType -> Type) (et :: EType) = DeclIndexVecF {unDeclIndexVecF :: Vec (DeclDimension et) (r EInt)}
 
 instance TR.HFunctor DeclIndexVecF where
   hfmap nat (DeclIndexVecF v) = DeclIndexVecF $ DT.map nat v
@@ -124,7 +128,7 @@ instance TR.HTraversable DeclIndexVecF where
   hmapM natM = fmap DeclIndexVecF . traverse natM . unDeclIndexVecF
   htraverse natM = fmap DeclIndexVecF . traverse natM . unDeclIndexVecF
 
-newtype IndexVecF (r :: EType -> Type) (et :: EType) = IndexVecF { unIndexVecF :: Vec (Dimension et) (r EInt) }
+newtype IndexVecF (r :: EType -> Type) (et :: EType) = IndexVecF {unIndexVecF :: Vec (Dimension et) (r EInt)}
 
 instance TR.HFunctor IndexVecF where
   hfmap nat (IndexVecF v) = IndexVecF $ DT.map nat v
@@ -133,7 +137,7 @@ instance TR.HTraversable IndexVecF where
   hmapM natM = fmap IndexVecF . traverse natM . unIndexVecF
   htraverse natM = fmap IndexVecF . traverse natM . unIndexVecF
 
-newtype IndexVecM (r :: EType -> Type) (et :: EType) = IndexVecM { unIndexVecM :: Vec (Dimension et) (Maybe (r EInt)) }
+newtype IndexVecM (r :: EType -> Type) (et :: EType) = IndexVecM {unIndexVecM :: Vec (Dimension et) (Maybe (r EInt))}
 
 instance TR.HFunctor IndexVecM where
   hfmap nat (IndexVecM v) = IndexVecM $ DT.map (fmap nat) v
@@ -149,7 +153,7 @@ data NestedVec :: Nat -> Type -> Type where
 
 instance Functor (NestedVec n) where
   fmap f = \case
-    NestedVec1 v ->  NestedVec1 $ DT.map f v
+    NestedVec1 v -> NestedVec1 $ DT.map f v
     NestedVec2 v -> NestedVec2 $ DT.map (DT.map f) v
     NestedVec3 v -> NestedVec3 $ DT.map (DT.map (DT.map f)) v
 
@@ -157,7 +161,7 @@ instance Foldable (NestedVec n) where
   foldMap f = \case
     NestedVec1 v -> foldMap f v
     NestedVec2 v -> mconcat $ DT.toList $ fmap (foldMap f) v
-    NestedVec3 v -> mconcat $ concatMap DT.toList $ DT.toList  $ fmap (foldMap f) <$> v
+    NestedVec3 v -> mconcat $ concatMap DT.toList $ DT.toList $ fmap (foldMap f) <$> v
 
 instance Traversable (NestedVec n) where
   traverse f = \case
@@ -169,7 +173,6 @@ unNest :: NestedVec n a -> ([Int], [a])
 unNest (NestedVec1 v) = ([DT.length v], DT.toList v)
 unNest (NestedVec2 v) = ([DT.length v, DT.length (DT.head v)], concatMap DT.toList $ DT.toList v)
 unNest (NestedVec3 v) = ([DT.length v, DT.length (DT.head v), DT.length (DT.head (DT.head v))], concat $ concatMap (fmap DT.toList . DT.toList) (DT.toList v))
-
 
 {-
 eTypeDim :: SType e -> Nat
--- haskell-stan/src/Stan/ModelBuilder/TypedExpressions/Program.hs
+++ haskell-stan/src/Stan/ModelBuilder/TypedExpressions/Program.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds #-}
@@ -13,18 +13,61 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 
-module Stan.ModelBuilder.TypedExpressions.Program
-  (
-    module Stan.ModelBuilder.TypedExpressions.Program
-  )
-  where
+module Stan.ModelBuilder.TypedExpressions.Program (
+    module Stan.ModelBuilder.TypedExpressions.Program,
+) where
 
-import qualified Stan.ModelBuilder.TypedExpressions.Statements as TB
-import qualified Stan.ModelBuilder.BuilderTypes as SBT
-import Relude.Extra
+import Data.Array ((!), (//))
 import qualified Data.Array as Array
+import qualified Data.List.NonEmpty as NE
+import Relude.Extra
+import qualified Stan.ModelBuilder.BuilderTypes as SBT
+import qualified Stan.ModelBuilder.TypedExpressions.Statements as TB
 
-newtype StanProgram = StanProgram { unStanProgram :: Array.Array SBT.StanBlock [TB.UStmt] }
+-- the StanBlock type has more sections so we can selectively add and remove things which are for
+-- only generated quantitied or the generation of log-likelihoods
+newtype StanProgram = StanProgram {unStanProgram :: Array.Array SBT.StanBlock [TB.UStmt]}
 
-programToStmt :: StanProgram -> TB.UStmt
-programToStmt = undefined
+programToStmt :: SBT.GeneratedQuantities -> StanProgram -> TB.UStmt
+programToStmt gq p = TB.SContext Nothing $ fullProgramStmt
+  where
+    stmtsArray = unStanProgram p
+    fullProgramStmt  =
+      let (s, ss1) = let d = dataStmt in maybe (d, []) (\x -> (x, [d])) functionsStmtM
+          ss2 = ss1 ++ maybe [] pure tDataStmtM
+          ss3 = ss2 ++ [paramsStmt]
+          ss4 = ss3 ++ maybe [] pure tParamsStmtM
+          ss5 = ss4 ++ [modelStmt]
+          ss6 = ss5 ++ maybe [] pure gqStmtM
+      in s :| ss6
+    functionsStmtM = let x = stmtsArray ! SBT.SBFunctions in if null x then Nothing else Just (TB.SBlock TB.FunctionsStmts x)
+    dataStmt =
+        let d = stmtsArray ! SBT.SBData
+            gqd = stmtsArray ! SBT.SBDataGQ
+         in TB.SBlock TB.DataStmts (d ++ if gq /= SBT.NoGQ then gqd else [])
+    tDataStmtM = let x = stmtsArray ! SBT.SBTransformedData in if null x then Nothing else Just (TB.SBlock TB.TDataStmts x)
+    paramsStmt = TB.SBlock TB.ParametersStmts $ stmtsArray ! SBT.SBParameters
+    tParamsStmtM = let x = stmtsArray ! SBT.SBTransformedParameters in if null x then Nothing else Just (TB.SBlock TB.TParametersStmts x)
+    modelStmt =
+        let ms = stmtsArray ! SBT.SBModel
+            gqms = stmtsArray ! SBT.SBGeneratedQuantities
+         in TB.SBlock TB.ModelStmts $ ms ++ if gq /= SBT.NoGQ then gqms else []
+    gqStmtM =
+        let gqs = stmtsArray ! SBT.SBGeneratedQuantities
+            lls = stmtsArray ! SBT.SBLogLikelihood
+         in case gq of
+                SBT.NoGQ -> Nothing
+                SBT.NoLL -> Just $ TB.SBlock TB.GeneratedQuantitiesStmts gqs
+                SBT.OnlyLL -> Just $ TB.SBlock TB.GeneratedQuantitiesStmts lls
+                SBT.All -> Just $ TB.SBlock TB.GeneratedQuantitiesStmts $ gqs ++ lls
+
+
+addToBlock :: SBT.StanBlock -> TB.UStmt -> Either Text (StanProgram -> StanProgram)
+addToBlock sb s = do
+  let f sp = StanProgram $ unStanProgram sp // [(sb, unStanProgram sp ! sb ++ [s])]
+  if sb `elem` [SBT.SBData, SBT.SBDataGQ, SBT.SBParameters]
+    then case s of
+           TB.SDeclare {} -> Right f
+           TB.SComment {} -> Right f
+           _ -> Left $ "Statement other than declaration or comment in data or parameters block."
+    else Right f
