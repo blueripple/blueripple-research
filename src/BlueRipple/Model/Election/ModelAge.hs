dc447154a87a64078e07b142c1737fa5589d5d88
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2022-11-28 21:16:15 -0500

    Working...

--- src/BlueRipple/Model/Election/ModelAge.hs
+++ src/BlueRipple/Model/Election/ModelAge.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module BlueRipple.Model.Election.ModelAge where
@@ -9,6 +12,9 @@ import qualified BlueRipple.Data.ACS_PUMS as PUMS
 import qualified BlueRipple.Data.DemographicTypes as DT
 import qualified BlueRipple.Data.ElectionTypes as ET
 import qualified BlueRipple.Data.Loaders as BRL
+import qualified BlueRipple.Utilities.KnitUtils as BRK
+import qualified BlueRipple.Data.DataFrames as BRDF
+import qualified BlueRipple.Data.Keyed as BRK
 
 import qualified Stan.ModelBuilder as S
 import qualified Stan.ModelBuilder.DesignMatrix as DM
@@ -17,34 +23,41 @@ import qualified Stan.ModelConfig as SC
 --import qualified Stan.ModelBuilder.TypedExpressions.Types as TE
 import qualified Stan.ModelBuilder.TypedExpressions.Expressions as TE
 
+import qualified Frames.MapReduce as FMR
+--import qualified Control.MapReduce as FMR
+
+import qualified Control.Foldl as FL
+import qualified Data.Vinyl as V
+import qualified Data.Vinyl.TypeLevel as V
 import qualified Frames as F
 import qualified Frames.Melt as F
+import qualified Knit.Report as K
 
 designMatrixRowACS :: forall rs.(F.ElemOf rs DT.CollegeGradC
-                                , F.ElemOf rs DT.InCollege
                                 , F.ElemOf rs DT.SexC
                                 , F.ElemOf rs DT.RaceAlone4C
                                 , F.ElemOf rs DT.HispC
                                 , F.ElemOf rs DT.PopPerSqMile
+                                , F.ElemOf rs BRDF.StateAbbreviation
+                                , F.ElemOf rs BRDF.CongressionalDistrict
                                 )
                    => DM.DesignMatrixRowPart (F.Record rs)
                    -> DM.DesignMatrixRow (F.Record rs)
-designMatrixRowACS densRP = DM.DesignMatrixRow (dmName dmType) [densRP, sexRP, eduRP, raceRP]
+designMatrixRowACS densRP = DM.DesignMatrixRow "ACSDMRow" [densRP, sexRP, eduRP, raceRP]
   where
     sexRP = DM.boundedEnumRowPart Nothing "Sex" (F.rgetField @DT.SexC)
-    eduRP = DM.boundedEnumRowPart Nothing "Education" (\r -> F.rgetField @DT.CollegeGradC r || F.rgetField @DT.InCollege r)
+    eduRP = DM.boundedEnumRowPart Nothing "Education" collegeGrad
     race5Census r = DT.race5FromRaceAlone4AndHisp True (F.rgetField @DT.RaceAlone4C r) (F.rgetField @DT.HispC r)
     raceRP = DM.boundedEnumRowPart (Just DT.R5_WhiteNonHispanic) "Race" race5Census
 
-groupBuilder :: [Text] -> [Text] -> S.StanGroupBuilderM (F.FrameRec PUMS.PUMS) () ()
-groupBuilder psGroup states cds = do
+groupBuilder :: [Text] -> [Text] -> S.StanGroupBuilderM (F.FrameRec ACSByCD) () ()
+groupBuilder states cds = do
   acsData <- S.addModelDataToGroupBuilder "ACS" (S.ToFoldable id)
   S.addGroupIndexForData stateGroup acsData $ S.makeIndexFromFoldable show (F.rgetField @DT.StateAbbreviation) states
   S.addGroupIndexForData cdGroup acsData $ S.makeIndexFromFoldable show districtKey cds
 
 setupACSRows :: (Typeable md, Typeable gq)
              => DM.DesignMatrixRowPart (F.Record ACSByCD)
-             -> (ET.OfficeT -> F.Record ACSByCD -> Double)
              -> S.StanBuilderM md gq (S.RowTypeTag (F.Record ACSByCD)
                                      , TE.IntArrayE
                                      , TE.MatrixE)
@@ -55,73 +68,68 @@ setupACSRows densRP = do
   dmACS <- DM.addDesignMatrix acsData dmRow (Just "DM")
   return (acsData, acsCit, dmACS)
 
-cdGroup :: SB.GroupTypeTag Text
-cdGroup = SB.GroupTypeTag "CD"
+cdGroup :: S.GroupTypeTag Text
+cdGroup = S.GroupTypeTag "CD"
 
-stateGroup :: SB.GroupTypeTag Text
-stateGroup = SB.GroupTypeTag "State"
+stateGroup :: S.GroupTypeTag Text
+stateGroup = S.GroupTypeTag "State"
 
 type Categoricals = [DT.Age5FC, DT.SexC, DT.CollegeGradC, DT.RaceAlone4C, DT.HispC]
 type ACSByCD = PUMS.CDCounts Categoricals
 type ACSByState = PUMS.StateCounts Categoricals
 
-acsByCD ∷ (K.KnitEffects r, BR.CacheEffects r)
+acsByCD ∷ (K.KnitEffects r, BRK.CacheEffects r)
         ⇒ F.FrameRec PUMS.PUMS
-        → F.FrameRec BR.DatedCDFromPUMA2012
+        → F.FrameRec BRL.DatedCDFromPUMA2012
         → K.Sem r (F.FrameRec ACSByCD)
 acsByCD acsByPUMA cdFromPUMA = fmap F.rcast <$> PUMS.pumsCDRollup (earliest earliestYear) (acsReKey . F.rcast) cdFromPUMA acsByPUMA
  where
   earliestYear = 2016
-  earliest year = (>= year) . F.rgetField @BR.Year
+  earliest year = (>= year) . F.rgetField @BRDF.Year
 
 cachedACSByCD
   ∷ ∀ r
-   . (K.KnitEffects r, BR.CacheEffects r)
+   . (K.KnitEffects r, BRK.CacheEffects r)
   ⇒ K.ActionWithCacheTime r (F.FrameRec PUMS.PUMS)
-  → K.ActionWithCacheTime r (F.FrameRec BR.DatedCDFromPUMA2012)
+  → K.ActionWithCacheTime r (F.FrameRec BRL.DatedCDFromPUMA2012)
   → K.Sem r (K.ActionWithCacheTime r (F.FrameRec ACSByCD))
 cachedACSByCD acs_C cdFromPUMA_C = do
-  let acsByCDDeps = (,) <$> pums_C <*> cdFromPUMA_C
-  BR.retrieveOrMakeFrame "model/age/acsByCD.bin" acsByCDDeps $
+  let acsByCDDeps = (,) <$> acs_C <*> cdFromPUMA_C
+  BRK.retrieveOrMakeFrame "model/age/acsByCD.bin" acsByCDDeps $
     \(acsByPUMA, cdFromPUMA) → acsByCD acsByPUMA cdFromPUMA
 
-acsByState ∷ F.FrameRec PUMS.PUMS → F.FrameRec PUMSByStateR
+acsByState ∷ F.FrameRec PUMS.PUMS → F.FrameRec ACSByState
 acsByState acsByPUMA = F.rcast <$> FL.fold (PUMS.pumsStateRollupF (acsReKey . F.rcast)) filteredACSByPUMA
  where
   earliestYear = 2016
-  earliest year = (>= year) . F.rgetField @BR.Year
+  earliest year = (>= year) . F.rgetField @BRDF.Year
   filteredACSByPUMA = F.filterFrame (earliest earliestYear) acsByPUMA
 
 cachedACSByState
   ∷ ∀ r
-   . (K.KnitEffects r, BR.CacheEffects r)
+   . (K.KnitEffects r, BRK.CacheEffects r)
   ⇒ K.ActionWithCacheTime r (F.FrameRec PUMS.PUMS)
-  → K.Sem r (K.ActionWithCacheTime r (F.FrameRec (StateKeyR V.++ CensusPredictorR V.++ '[PUMS.Citizens])))
-cachedACSByState pums_C = do
-  let zeroCount ∷ F.Record '[PUMS.Citizens]
-      zeroCount = 0 F.&: V.RNil
-      addZeroF =
-        FMR.concatFold $
-          FMR.mapReduceFold
-            FMR.noUnpack
-            (FMR.assignKeysAndData @StateKeyR @(Categoricals V.++ '[PUMS.Citizens]))
-            ( FMR.makeRecsWithKey id $
-                FMR.ReduceFold $
-                  const $
-                    BRK.addDefaultRec @Categoricals zeroCount
-            )
-  BR.retrieveOrMakeFrame "model/age/acsByState.bin" pums_C $
-    pure . FL.fold addZeroF . pumsByState
+  → K.Sem r (K.ActionWithCacheTime r (F.FrameRec ACSByState))
+cachedACSByState acs_C = do
+  BRK.retrieveOrMakeFrame "model/age/acsByState.bin" acs_C $
+    pure . acsByState
 
 acsReKey
   ∷ F.Record '[DT.Age5FC, DT.SexC, DT.CollegeGradC, DT.InCollege, DT.RaceAlone4C, DT.HispC]
   → F.Record Categoricals
 acsReKey r =
-  let cg = F.rgetField @DT.CollegeGradC r
-      ic = F.rgetField @DT.InCollege r
-  in F.rgetField @DT.Age5FC r
-     F.&: F.rgetField @DT.SexC r
-     F.&: (if cg == DT.Grad || ic then DT.Grad else DT.NonGrad)
-     F.&: F.rgetField @DT.RaceAlone4C r
-     F.&: F.rgetField @DT.HispC r
-     F.&: V.RNil
+  F.rgetField @DT.Age5FC r
+  F.&: F.rgetField @DT.SexC r
+  F.&: (if collegeGrad r || inCollege r then DT.Grad else DT.NonGrad)
+  F.&: F.rgetField @DT.RaceAlone4C r
+  F.&: F.rgetField @DT.HispC r
+  F.&: V.RNil
+
+collegeGrad :: F.ElemOf rs DT.CollegeGradC => F.Record rs -> Bool
+collegeGrad r = F.rgetField @DT.CollegeGradC r == DT.Grad
+
+inCollege :: F.ElemOf rs DT.InCollege => F.Record rs -> Bool
+inCollege = F.rgetField @DT.InCollege
+
+districtKey :: (F.ElemOf rs BRDF.StateAbbreviation, F.ElemOf rs BRDF.CongressionalDistrict) => F.Record rs -> Text
+districtKey r = F.rgetField @BRDF.StateAbbreviation r <> "-" <> show (F.rgetField @BRDF.CongressionalDistrict r)
