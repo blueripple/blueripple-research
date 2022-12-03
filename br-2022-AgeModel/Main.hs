08e22e21c9033105dacf6f3439f3ec0d287e2224
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2022-12-03 09:29:01 -0500

    Added app directory for AgeModel testing

---
+++ br-2022-AgeModel/Main.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Main where
+
+import BlueRipple.Model.Election.AgeModel
+
+main :: IO ()
+main = pure ()
