e86be60fa5018f4d4ff1cfc6c38a6625d247ecb4
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2022-12-11 23:05:25 -0500

    Working...

--- br-2022-AgeModel/Main.hs
+++ br-2022-AgeModel/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
@@ -30,6 +31,7 @@ import qualified Knit.Effect.AtomicCache as K (cacheTime)
 import qualified Knit.Effect.AtomicCache as KC
 import qualified Text.Pandoc.Error as Pandoc
 import qualified System.Console.CmdArgs as CmdArgs
+import qualified Polysemy
 
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -38,6 +40,9 @@ import qualified Data.Vector.Unboxed as VU
 import qualified Control.Foldl as FL
 import qualified Frames as F
 import Control.Lens (view)
+
+import Path (Dir, Rel)
+import qualified Path
 
 import qualified Frames.Visualization.VegaLite.Data as FVD
 import qualified Graphics.Vega.VegaLite as GV
@@ -74,12 +79,52 @@ main = do
           , K.persistCache = KC.persistStrictByteString (\t → toString (cacheDir <> "/" <> t))
           }
   resE ← K.knitHtmls knitConfig $ do
+    K.logLE K.Info $ "Command Line: " <> show cmdLine
     runAgeModel False
-    runEduModel False
+    runEduModel False cmdLine
   case resE of
     Right namedDocs →
       K.writeAllPandocResultsWithInfoAsHtml "" namedDocs
     Left err → putTextLn $ "Pandoc Error: " <> Pandoc.renderError err
+
+-- emptyRel = [Path.reldir||]
+postDir ∷ Path.Path Rel Dir
+postDir = [Path.reldir|br-2022-AgeModel/posts|]
+
+postLocalDraft
+  ∷ Path.Path Rel Dir
+  → Maybe (Path.Path Rel Dir)
+  → Path.Path Rel Dir
+postLocalDraft p mRSD = case mRSD of
+  Nothing → postDir BR.</> p BR.</> [Path.reldir|draft|]
+  Just rsd → postDir BR.</> p BR.</> rsd
+
+postInputs ∷ Path.Path Rel Dir → Path.Path Rel Dir
+postInputs p = postDir BR.</> p BR.</> [Path.reldir|inputs|]
+
+sharedInputs ∷ Path.Path Rel Dir
+sharedInputs = postDir BR.</> [Path.reldir|Shared|] BR.</> [Path.reldir|inputs|]
+
+postOnline ∷ Path.Path Rel t → Path.Path Rel t
+postOnline p = [Path.reldir|research/NewMaps|] BR.</> p
+
+postPaths
+  ∷ (K.KnitEffects r, MonadIO (K.Sem r))
+  ⇒ Text
+  → BR.CommandLine
+  → K.Sem r (BR.PostPaths BR.Abs)
+postPaths t cmdLine = do
+  let mRelSubDir = case cmdLine of
+        BR.CLLocalDraft _ _ mS _ → maybe Nothing BR.parseRelDir $ fmap toString mS
+        _ → Nothing
+  postSpecificP ← K.knitEither $ first show $ Path.parseRelDir $ toString t
+  BR.postPaths
+    BR.defaultLocalRoot
+    sharedInputs
+    (postInputs postSpecificP)
+    (postLocalDraft postSpecificP mRelSubDir)
+    (postOnline postSpecificP)
+
 
 logLengthC :: (K.KnitEffects r, Foldable f) => K.ActionWithCacheTime r (f a) -> Text -> K.Sem r ()
 logLengthC xC t = K.ignoreCacheTime xC >>= \x -> K.logLE K.Info $ t <> "has " <> show (FL.fold FL.length x) <> " rows."
@@ -114,8 +159,8 @@ runAgeModel clearCaches = do
       (pure ())
   K.logLE K.Info "Test run complete."
 
-runEduModel :: (K.KnitEffects r, BRK.CacheEffects r) => Bool -> K.Sem r ()
-runEduModel clearCaches = do
+runEduModel :: (K.KnitEffects r, K.KnitMany r, BRK.CacheEffects r) => Bool -> BR.CommandLine → K.Sem r ()
+runEduModel clearCaches cmdLine = do
   let cacheKeyE = let k = "model/AgeModel/test" in if clearCaches then Left k else Right k
       runnerInputNames = SC.RunnerInputNames
                          "br-2022-AgeModel/stanEdu"
@@ -123,6 +168,8 @@ runEduModel clearCaches = do
                          Nothing
                          "acsEdu"
       only2020 r = F.rgetField @BRDF.Year r == 2020
+      postInfo = BR.PostInfo (BR.postStage cmdLine) (BR.PubTimes BR.Unpublished Nothing)
+  eduModelPaths <- postPaths "EduModel" cmdLine
   acs_C <- fmap (F.filterFrame only2020) <$> PUMS.pumsLoader Nothing >>= AM.cachedACSByState
   logLengthC acs_C "acsByState"
   let acsMN_C = fmap AM.acsByStateEduMN acs_C
@@ -131,8 +178,11 @@ runEduModel clearCaches = do
   (dw, code) <- SMR.dataWranglerAndCode acsMN_C (pure ())
                 (AM.groupBuilderState (S.toList states))
                 (AM.binomialModel (AM.designMatrixRowEdu2 Nothing)) -- (Just AM.logDensityDMRP)
+  acsMN <- K.ignoreCacheTime acsMN_C
+  BRK.brNewPost eduModelPaths postInfo "EduModel" $ do
+    _ <- K.addHvega Nothing Nothing $ chart (FV.ViewConfig 100 500 5) acsMN
+    pure ()
   () <- do
-    K.ignoreCacheTimeM acsMN_C >>= (K.addHvega Nothing Nothing $ chart (FV.ViewConfig 500 500 5))
     K.ignoreCacheTimeM
       $ SMR.runModel' @BRK.SerializerC @BRK.CacheData
       cacheKeyE
@@ -150,6 +200,7 @@ chart vc rows =
   let total v = v VU.! 0 + v VU.! 1
       grads v = v VU.! 1
       rowToData (r, v) = [("Sex", GV.Str $ show $ F.rgetField @DT.SexC r)
+                         , ("State", GV.Str $ F.rgetField @BRDF.StateAbbreviation r)
                          , ("Age", GV.Str $ show $ F.rgetField @DT.Age5FC r)
                          , ("Race", GV.Str $ show (F.rgetField @DT.RaceAlone4C r) <> "_" <> show (F.rgetField @DT.HispC r))
                          , ("Total", GV.Number $ realToFrac $ total v)
@@ -158,8 +209,12 @@ chart vc rows =
                          ]
       toVLDataRows x = GV.dataRow (rowToData x) []
       vlData = GV.dataFromRows [] $ concat $ fmap toVLDataRows $ FL.fold FL.list rows
-      encX = GV.position GV.X [GV.PName "Age", GV.PmType GV.Nominal]
-      encY = GV.position GV.Y [GV.PName "FracGrad", GV.PmType GV.Quantitative]
+      encAge = GV.position GV.X [GV.PName "Age", GV.PmType GV.Nominal]
+      encSex = GV.color [GV.MName "Sex", GV.MmType GV.Nominal]
+      encState = GV.row [GV.FName "State", GV.FmType GV.Nominal]
+      encRace = GV.column [GV.FName "Race", GV.FmType GV.Nominal]
+      encFracGrad = GV.position GV.Y [GV.PName "FracGrad", GV.PmType GV.Quantitative]
+      encTotal = GV.size [GV.MName "Total", GV.MmType GV.Quantitative]
       mark = GV.mark GV.Circle []
-      enc = (GV.encoding . encX . encY)
+      enc = (GV.encoding . encAge . encSex . encFracGrad . encRace . encTotal)
   in FV.configuredVegaLite vc [FV.title "FracGrad v Age", enc [], mark, vlData]
