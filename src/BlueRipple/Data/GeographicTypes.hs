5f61ba3886b6ff99f41e3ef594cb96cdc13cc05d
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2023-01-23 13:06:39 -0500

    Added untracked files

---
+++ src/BlueRipple/Data/GeographicTypes.hs
@@ -0,0 +1,164 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module BlueRipple.Data.GeographicTypes
+  (
+    module BlueRipple.Data.GeographicTypes
+--  , module BlueRipple.Data.DataFrames
+  )
+  where
+
+import qualified BlueRipple.Data.Keyed         as K
+import Data.Discrimination (Grouping)
+import qualified Data.Text.Read as TR
+import qualified Data.Vector.Unboxed as UVec
+import Data.Vector.Unboxed.Deriving (derivingUnbox)
+import qualified Data.Vinyl as V
+import qualified Flat
+import qualified Frames as F
+import qualified Frames.Streamly.InCore as FSI
+import qualified Frames.Streamly.TH as FTH
+import qualified Frames.ShowCSV as FCSV
+import qualified Frames.Visualization.VegaLite.Data as FV
+import qualified Graphics.Vega.VegaLite as GV
+
+FTH.declareColumn "StateName" ''Text
+
+FTH.declareColumn "StateAbbreviation" ''Text
+
+FTH.declareColumn "StateFIPS" ''Int
+
+FTH.declareColumn "CountyFIPS" ''Int
+
+FTH.declareColumn "PUMA" ''Int
+
+FTH.declareColumn "CongressionalDistrict" ''Int
+
+data DistrictType = Congressional | StateUpper | StateLower deriving stock (Show, Enum, Bounded, Eq, Ord, Generic)
+
+--instance S.Serialize DistrictType
+--instance B.Binary DistrictType
+instance Flat.Flat DistrictType
+instance Grouping DistrictType
+instance FCSV.ShowCSV DistrictType
+derivingUnbox
+  "DistrictType"
+  [t|DistrictType -> Word8|]
+  [|toEnum . fromEnum|]
+  [|toEnum . fromEnum|]
+
+type instance FSI.VectorFor DistrictType = UVec.Vector
+FTH.declareColumn "DistrictTypeC" ''DistrictType
+
+instance FV.ToVLDataValue (F.ElField DistrictTypeC) where
+  toVLDataValue x = (toText $ V.getLabel x, GV.Str $ show $ V.getField x)
+
+--FS.declareColumn "DistrictNumber" ''Int
+FTH.declareColumn "DistrictName" ''Text
+
+-- if integer part and text part, compare using integer part first, then text.  Else just use text.
+districtNameCompare :: Text -> Text -> Ordering
+districtNameCompare t1 t2 = --if compInt == EQ then compare tr1 tr2 else compInt where
+  let parsed1 = TR.decimal t1
+      parsed2 = TR.decimal t2
+  in case (parsed1, parsed2) of
+    (Right (n1 :: Int, tr1), Right (n2, tr2)) -> if n1 == n2 then compare tr1 tr2 else compare n1 n2
+    _ -> compare t1 t2
+{-# INLINEABLE districtNameCompare #-}
+
+
+-- Serialize for caching
+-- FI.VectorFor for frames
+-- Grouping for leftJoin
+data CensusRegion = Northeast | Midwest | South | West | OtherRegion deriving stock (Show, Enum, Bounded, Eq, Ord, Generic)
+deriving anyclass instance Hashable CensusRegion
+
+--instance S.Serialize CensusRegion
+--instance B.Binary CensusRegion
+instance Flat.Flat CensusRegion
+instance Grouping CensusRegion
+instance K.FiniteSet CensusRegion
+derivingUnbox "CensusRegion"
+  [t|CensusRegion -> Word8|]
+  [|toEnum . fromEnum|]
+  [|toEnum . fromEnum|]
+type instance FSI.VectorFor CensusRegion = UVec.Vector
+
+--type CensusRegionC = "CensusRegion" F.:-> CensusRegion
+FTH.declareColumn "CensusRegionC" ''CensusRegion
+
+data CensusDivision = NewEngland
+                    | MiddleAtlantic
+                    | EastNorthCentral
+                    | WestNorthCentral
+                    | SouthAtlantic
+                    | EastSouthCentral
+                    | WestSouthCentral
+                    | Mountain
+                    | Pacific
+                    | OtherDivision
+                  deriving stock (Show, Enum, Bounded, Eq, Ord, Generic)
+deriving anyclass instance Hashable CensusDivision
+
+--instance S.Serialize CensusDivision
+--instance B.Binary CensusDivision
+instance Flat.Flat CensusDivision
+instance Grouping CensusDivision
+instance K.FiniteSet CensusDivision
+derivingUnbox "CensusDivision"
+  [t|CensusDivision -> Word8|]
+  [|toEnum . fromEnum|]
+  [|toEnum . fromEnum|]
+type instance FSI.VectorFor CensusDivision = UVec.Vector
+
+--type CensusDivisionC = "CensusDivision" F.:-> CensusDivision
+FTH.declareColumn "CensusDivisionC" ''CensusDivision
+
+censusDivisionToRegion :: CensusDivision -> CensusRegion
+censusDivisionToRegion NewEngland = Northeast
+censusDivisionToRegion MiddleAtlantic = Northeast
+censusDivisionToRegion EastNorthCentral = Midwest
+censusDivisionToRegion WestNorthCentral = Midwest
+censusDivisionToRegion SouthAtlantic = South
+censusDivisionToRegion EastSouthCentral = South
+censusDivisionToRegion WestSouthCentral = South
+censusDivisionToRegion Mountain = West
+censusDivisionToRegion Pacific = West
+censusDivisionToRegion OtherDivision = OtherRegion
+
+data CensusMetro = MetroUnknown
+                 | NonMetro
+                 | MetroPrincipal
+                 | MetroOther
+                 | MetroMixed deriving stock (Show, Enum, Bounded, Eq, Ord, Generic)
+deriving anyclass instance Hashable CensusMetro
+
+--instance S.Serialize CensusMetro
+--instance B.Binary CensusMetro
+instance Flat.Flat CensusMetro
+instance Grouping CensusMetro
+instance K.FiniteSet CensusMetro
+derivingUnbox "CensusMetro"
+  [t|CensusMetro -> Word8|]
+  [|toEnum . fromEnum|]
+  [|toEnum . fromEnum|]
+type instance FSI.VectorFor CensusMetro = UVec.Vector
+
+--type CensusMetroC = "CensusMetro" F.:-> CensusMetro
+FTH.declareColumn "CensusMetroC" ''CensusMetro
+
+
+--type PctInMetro = "PctIntMetro" F.:-> Double
+FTH.declareColumn "PctInMetro" ''Double
---
+++ src/BlueRipple/Model/Demographic/EnrichCensus.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UnicodeSyntax #-}
+--{-# LANGUAGE NoStrictData #-}
+
+module BlueRipple.Model.Demographic.EnrichCensus
+  (
+    module BlueRipple.Model.Demographic.EnrichCensus
+  )
+where
+
+import qualified BlueRipple.Data.DemographicTypes as DT
+import qualified BlueRipple.Model.Demographic.StanModels as SM
+import qualified BlueRipple.Model.Demographic.DataPrep as DDP
+import qualified BlueRipple.Model.Demographic.EnrichData as DED
+import qualified BlueRipple.Data.Keyed as Keyed
+
+import qualified BlueRipple.Data.CensusLoaders as BRC
+import qualified BlueRipple.Data.CensusTables as BRC
+
+import qualified BlueRipple.Data.ACS_PUMS as PUMS
+import qualified BlueRipple.Data.DemographicTypes as DT
+import qualified BlueRipple.Data.DataFrames as BRDF
+import qualified BlueRipple.Utilities.KnitUtils as BRK
+
+import qualified Knit.Report as K
+import qualified Knit.Utilities.Streamly as KS
+
+import qualified Stan.ModelRunner as SMR
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vinyl.TypeLevel as V
+import qualified Control.Foldl as FL
+import qualified Frames as F
+import qualified Frames.Transform as FT
+
+import Control.Lens (view)
+
+type CensusASERR = BRC.CensusRow BRC.LDLocationR BRC.ExtensiveDataR [DT.SimpleAgeC, DT.SexC, DT.Education4C, BRC.RaceEthnicityC]
+type CensusASERRecodedR = BRC.LDLocationR
+                          V.++ BRC.ExtensiveDataR
+                          V.++ [DT.SimpleAgeC, DT.SexC, DT.CollegeGradC, DT.RaceAlone4C, DT.HispC, DT.PopCount, DT.PopPerSqMile]
+
+
+{-
+-- Step 1
+-- NB all this data is without a year field. So this step needs to be done for each year
+enrichCensusDataViaStateAgeModel :: SM.AgeStateModelResult -> BRC.LoadedCensusTablesByLD -> K.Sem r (F.FrameRec CensusASERR)
+enrichCensusDataViaStateAgeModel amr censusTables = do
+  enrichedViaModel <- KS.streamlyToKnit
+                      $ DED.enrichFrameFromBinaryModel @DT.SimpleAge @BRC.Count
+                      amr
+                      (F.rgetField @BRDF.StateAbbreviation)
+                      DT.EqualOrOver
+                      DT.Under
+                      (BRC.sexEducationRace censusTables)
+  let rowSumFld = DED.desiredRowSumsFld @DT.Age4C @BRC.Count @[BRDF.StateAbbreviation, DT.SexC, DT.RaceAlone4C, DT.HispC] allSimpleAges DT.age4ToSimple
+  let ncFld = DED.nearestCountsFrameFld @BRC.Count @DT.SimpleAgeC @DT.Education4C (DED.nearestCountsFrameIFld DED.nearestCountsKL) desiredRowSumLookup allEdus
+-}
