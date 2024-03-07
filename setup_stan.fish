23041218fe2897f3b9f165ff8a5bda21b6fa769b
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2024-03-06 19:08:17 -0500

    Added env variables settings for data in new libs

--- setup_stan.fish
+++ setup_stan.fish
@@ -2,3 +2,7 @@ set -gx MACOSX_DEPLOYMENT_TARGET 14.0
 set -gx CMDSTAN_DIR ~/DataScience/cmdstan-2.34.1
 set -gx CLANG_BINDIR /usr/bin/clang
 set -gx STAN_THREADS 1
+set -gx BR_SMALL_DATA ~/BlueRipple/libs/small-data-sets/data/
+set -gx BR_ACS_PUMS_DATA_DIR ~/BlueRipple/bigData/IPUMS/
+set -gx BR_CES_DATA_DIR ~/BlueRipple/bigData/CCES/
+set -gx BR_CPS_VS_DATA_DIR ~/BlueRipple/bigData/IPUMS/
